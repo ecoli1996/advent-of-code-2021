bb6e490bc40b5f59471f02745309d5d2232a4dc9
Elaina Cole <coleev@g.cofc.edu>
2021-12-06 19:26:39 -0500

    Update ProgamPart1.cs

--- day2/ProgamPart1.cs
+++ day2/ProgamPart1.cs
@@ -7,26 +7,22 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            var lines = System.IO.File.ReadAllLines(@"C:\aoc_day2.txt").Select(line => GetLine(line)).ToArray();
+            var lines = System.IO.File.ReadAllLines(@"C:\aoc_day2.txt");
             var depth = 0;
             var horizontalPosition = 0;
 
-            foreach (Tuple<Direction, int> line in lines)
+            foreach (string line in lines)
             {
-                if (line.Item1 == Direction.down) depth += line.Item2;
-                else if (line.Item1 == Direction.forward) horizontalPosition += line.Item2;
-                else depth -= line.Item2;
+                var splitLine = line.Split(' ');
+                var direction = Enum.Parse<Direction>(splitLine[0]);
+                var units = int.Parse(splitLine[1]);
+
+                if (direction == Direction.down) depth += units;
+                else if (direction == Direction.forward) horizontalPosition += units;
+                else depth -= units;
             }
 
             Console.WriteLine($"Depth: {depth}\nPosition: {horizontalPosition}\nProduct: {depth * horizontalPosition}");
-        }
-
-        private static Tuple<Direction, int> GetLine(string line)
-        {
-            var lineComponents = line.Split(' ');
-            var direction = Enum.Parse<Direction>(lineComponents[0]);
-            var units = int.Parse(lineComponents[1]);
-            return new Tuple<Direction, int>(direction, units);
         }
 
         private enum Direction
